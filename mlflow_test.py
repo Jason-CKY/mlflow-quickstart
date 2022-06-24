28d823b3ad6a64e7515d9422761c9a7f5586ea14
Jason Cheng <jasoncky96@gmail.com>
2022-06-24 21:13:53 +0800

    Set pytorch image

--- mlflow_test.py
+++ mlflow_test.py
@@ -1,8 +1,16 @@
 import os
+import numpy as np
 import tempfile
 from pprint import pprint
 
 import mlflow
+
+metrics = {
+    "test-int": 2,
+    "test-float": 4.23,
+    'test-np': np.float64(2.41245345)
+}
+
 
 def save_text(path, text):
     with open(path, "w") as f:
@@ -19,7 +27,7 @@ def main():
         save_text(tmp_path_b, "1")
         mlflow.log_artifact(tmp_path_a)
         mlflow.log_artifacts(tmp_sub_dir, artifact_path="dir")
-        mlflow.log_metrics({"test": 2})
+        mlflow.log_metrics(metrics)
 
     client = mlflow.tracking.MlflowClient()
     client.delete_run(run.info.run_id)
@@ -33,7 +41,7 @@ def main():
         save_text(tmp_path_b, "1")
         mlflow.log_artifact(tmp_path_a)
         mlflow.log_artifacts(tmp_sub_dir, artifact_path="dir")
-        mlflow.log_metrics({"test": 2})
+        mlflow.log_metrics(metrics)
 
     # Downloading artifacts
     pprint(os.listdir(client.download_artifacts(run.info.run_id, "")))
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Mlflow quick-setup
 
 Quickly setup mlflow with remote tracking server and s3 artifact store. Just run `docker-compose up` to get started.
+
+```bash
+source .env
+MLFLOW_TRACKING_URI=http://localhost:5000 mlflow runs list --experiment-id 0 --view deleted_only
+MLFLOW_TRACKING_URI=http://localhost:5000 mlflow gc --backend-store-uri postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DB}  --run-ids 
+
+```
